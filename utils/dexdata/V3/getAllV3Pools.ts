b00acc3776382683f6911caa320b3154b1d431e1
prophecy <prophecyfulfilment@gmail.com>
2023-09-26 19:59:45 +0100

    pool object work

--- utils/dexdata/V3/getAllV3Pools.ts
+++ utils/dexdata/V3/getAllV3Pools.ts
@@ -8,9 +8,10 @@ import { Pool, FeeAmount, Tick } from '@uniswap/v3-sdk';
 import { TickMath } from '@uniswap/v3-sdk';
 import { TickDataProvider } from '@uniswap/v3-sdk';
 import { TickProvider } from '../../../scripts/v3/modules/TickProvider';
+import JSBI from 'jsbi';
 import { Token } from '@uniswap/sdk-core';
 import { parse } from 'path';
-
+import { fitFee } from './fitFee';
 
 export async function getAllV3Pools() {
 	// Get the addresses of all the deployed UNISWAP V3 factories
@@ -41,32 +42,54 @@ export async function getAllV3Pools() {
 				const parsedEvent = factory.interface.parseLog(event);
 				if (parsedEvent.args.token0 != undefined && parsedEvent.args.token1 != undefined) {
 					//Verify undefined results are cleaned up
+
 					console.log('parsedEvent', parsedEvent.args.token0, parsedEvent.args.token1, parsedEvent.args.pool);
+					const t0 = new ethers.Contract(parsedEvent.args.token0, IERC20, provider);
+					const t1 = new ethers.Contract(parsedEvent.args.token1, IERC20, provider);
 					const v3Pool = new ethers.Contract(parsedEvent.args.pool, IUniswapV3Pool, wallet);
+					const token0 = new Token(137, await parsedEvent.args.token0, await t0.decimals(), await t0.symbol());
+					const token1 = new Token(137, await parsedEvent.args.token1, await t1.decimals(), await t1.symbol());
+
+					const fee = await v3Pool.fee();
+					const feeAmount = await fitFee(fee);
+
+					console.log('fee', fee)
 					const slot0 = await v3Pool.slot0();
-					const tick = TickMath.getTickAtSqrtRatio(slot0.sqrtRatioX96);
-					const tickProvider = new TickProvider(v3Pool);
+					console.log('slot0', slot0)
+					const tickSpacing = await v3Pool.tickSpacing();
+					console.log('tickSpacing', tickSpacing)
+					const tick = slot0.tick;
+
+					// // Possibly useful later, but apparently not useful just now.
+					// const tick = TickMath.getTickAtSqrtRatio(slot0.sqrtRatioX96);
+					// const tickProvider = new TickProvider(v3Pool);
 
 					const deployedPool = new Pool(
-						await v3Pool.token0(),
-						await v3Pool.token1(),
-						await v3Pool.fee(),
+						token0,
+						token1,
+						feeAmount,
 						slot0.sqrtPriceX96,
 						await v3Pool.liquidity(),
-						tick,
-						tickProvider);
-					console.log('deployedPool', deployedPool)
-					deployedPools.push(deployedPool); // add the new Pool object to the deployedPools array
-					return deployedPool;
-				} else {
-					return undefined;
-				}
-			});
-			const deployedPools = await Promise.all(poolAddresses);
-			console.log('poolAddresses', deployedPools);
-		}
-	};
-}
+						await slot0.tick,
+					);
 
+					console.log('deployedPool', deployedPool);
+					deployedPools.push(deployedPool); // add the new Pool object to the deployedPools array
+				}
+
+				console.log('poolAddresses', deployedPools);
+			});
+		}
+	}
+}
 getAllV3Pools();
 
+
+// const fee = FeeAmount.MEDIUM;
+// const sqrtRatioX96 = parsedEvent.args.sqrtPriceX96;
+// const liquidity = parsedEvent.args.liquidity;
+// const tickCurrent = parsedEvent.args.tick;
+// const ticks: Tick[] = [];
+// const deployedPool = new Pool(token0, token1, fee, sqrtRatioX96, liquidity, tickCurrent, ticks);
+// deployedPools.push(deployedPool); // add the new Pool object to the deployedPools array
+// return deployedPool;
