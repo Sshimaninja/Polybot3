f33ee61471aa7fdb89918d0e56b793c9cc1a0295
prophecy <prophecyfulfilment@gmail.com>
2023-11-04 18:55:32 +0000

    got v3inRangeReserves working

--- scripts/v3/swapv3.ts
+++ scripts/v3/swapv3.ts
@@ -1,15 +1,9 @@
 require('dotenv').config()
 require('colors')
 import { BigNumber as BN } from "bignumber.js";
-import { Prices } from './modules/prices';
 import { FactoryPair, Pair } from '../../constants/interfaces';
-import { AmountCalculator } from './modules/amountCalcSingle'
 import { Trade } from './modules/getV3Trade';
-import { gasVprofit } from './modules/gasVprofit';
-import { InRangeLiquidity } from './modules/reserves';
-import { sendit } from './execute';
-import { tradeLogs } from './modules/tradeLog';
-import { rollDamage } from './modules/damage';
+import { InRangeLiquidity, Match3Pools, V3Matches } from './modules/reserves';
 /*
 TODO:
 */
@@ -27,29 +21,40 @@ const slippageTolerance = BN(0.0006) // 0.065%
 // var virtualReserveFactor = 1.1
 var pendingID: string | undefined
 
-export async function control(data: FactoryPair[], gasData: any) {
+
+export async function control(data: V3Matches, gasData: any) {
 	const promises: any[] = [];
-
-	for (const pair of data) {
-		console.log("ExchangeA: " + pair.exchangeA + " ExchangeB: " + pair.exchangeB + " matches: " + pair.matches.length, " gasData: " + gasData.fast.maxFee + " " + gasData.fast.maxPriorityFee);
-
-		for (const match of pair.matches) {
-			if (!tradePending && match.poolA_id !== pendingID && match.poolB_id !== pendingID) {
+	const matches: V3Matches = data;
+	const pools: Match3Pools[] = data.matches;
+	console.log("matches: " + matches.matches.length);
 
 
-				const t = new Trade(pair, match, gasData);
-				const trade = await t.getTrade();
+	for (const pool of pools) {
+		console.log("ExchangeA: " + matches.exchangeA + " ExchangeB: " + matches.exchangeB + " matches: " + matches.matches.length, " gasData: " + gasData.fast.maxFee + " " + gasData.fast.maxPriorityFee);
 
-				const dataPromise = tradeLogs(trade);
-				const rollPromise = rollDamage(trade, await dataPromise, warning, tradePending, pendingID);
+		for (const match of matches.matches) {
+			console.log("Getting Reserves and Data for Match: " + match.ticker + " " + match.poolID0.id + " " + match.poolID1.id)
+			if (!tradePending && match.poolID0.id !== pendingID && match.poolID1.id !== pendingID) {
 
-				promises.push(dataPromise, rollPromise);
+
+				const r = new InRangeLiquidity(match);
+				const inRange = await r.getLiquidity();
+
+
+
+				// const t = new Trade(pair, match, gasData);
+				// const trade = await t.getTrade();
+
+				// const dataPromise = tradeLogs(trade);
+				// const rollPromise = rollDamage(trade, await dataPromise, warning, tradePending, pendingID);
+
+				// promises.push(dataPromise, rollPromise);
 			}
 		}
 	}
 
-	await Promise.all(promises).catch((error: any) => {
-		console.log("Error in control.ts: " + error.message);
-		return;
-	});
+	// await Promise.all(promises).catch((error: any) => {
+	// 	console.log("Error in control.ts: " + error.message);
+	// 	return;
+	// });
 }
