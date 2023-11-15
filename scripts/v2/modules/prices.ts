d033a6f25125d91eca0b544af9d99d9299f2327b
prophecy <prophecyfulfilment@gmail.com>
2023-11-15 16:53:00 +0000

    database++

--- scripts/v2/modules/prices.ts
+++ scripts/v2/modules/prices.ts
@@ -9,17 +9,19 @@ import { Token } from "../../../constants/interfaces";
  */
 
 export class Prices {
-    poolID: string;
-    priceInBN: BN;
-    priceOutBN: BN;
-    reserves: ReservesData;
-
-    constructor(token0: Token, token1: Token, poolID: string, reserves: ReservesData) {
-        this.poolID = poolID;
-        this.reserves = reserves;
-        reserves.reserveIn = reserves.reserveIn;
-        reserves.reserveOut = reserves.reserveOut;
-        this.priceInBN = reserves.reserveInBN.div(reserves.reserveOutBN);
-        this.priceOutBN = reserves.reserveOutBN.div(reserves.reserveInBN);
-    }
+	poolID: string;
+	priceInBN: BN;
+	priceOutBN: BN;
+	reserves: ReservesData;
+	constructor(poolID: string, reserves: ReservesData) {
+		if (!reserves) {
+			throw new Error('Reserves is undefined');
+		}
+		this.poolID = poolID;
+		this.reserves = reserves;
+		reserves.reserveIn = reserves.reserveIn;
+		reserves.reserveOut = reserves.reserveOut;
+		this.priceInBN = reserves.reserveInBN.div(reserves.reserveOutBN);
+		this.priceOutBN = reserves.reserveOutBN.div(reserves.reserveInBN);
+	}
 }
