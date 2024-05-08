ef530696ceb66bf52e68881360e1f0be9f283aba
Justin Boon <justinmichaelboon@gmail.com>
2024-05-08 17:39:02 +0200

    volToTarget

--- scripts/v3/modules/price/ref/calcVolToTarget.ts
+++ scripts/v3/modules/price/ref/calcVolToTarget.ts
@@ -1,9 +1,12 @@
 import { IRL, InRangeLiquidity, V3Reserves } from '../inRangeLiquidity';
 import { LiquidityMath, Pool, SwapMath, TickMath } from "@uniswap/v3-sdk";
 import { ERC20token, PoolInfo, PoolStateV3, Slot0 } from '../../../../../constants/interfaces';
+import { abi as IUniswapV3PoolState } from '@uniswap/v3-core/artifacts/contracts/interfaces/pool/IUniswapV3PoolState.sol/IUniswapV3PoolState.json';
+import { abi as IUniswapV3Pool } from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json';
 import { ethers } from 'ethers';
-import JSBI from '/root/polybotv3/node_modules/@uniswap/sdk-core/node_modules/jsbi/jsbi';
 import { Contract } from 'alchemy-sdk';
+import { provider } from '../../../../../constants/provider';
+
 
 // //  amount of x in range; sp = sqrt of current price, sb = sqrt of max price
 // def x_in_range(L, sp, sb):
@@ -15,7 +18,7 @@ import { Contract } from 'alchemy-sdk';
 
 // def tick_to_price(tick):
 // return 1.0001 ** tick
-export interface Tick {
+export interface ITick {
 	liquidityGross: bigint
 	liquidityNet: bigint
 	feeGrowthOutside0X128: bigint
@@ -25,37 +28,47 @@ export interface Tick {
 	secondsOutside: bigint
 	initialized: boolean
 }
-
+// contract - the pool's contract
+// sCurrentPrice - sqrt of the current price
+// sPriceTarget - sqrt of the target price
+// liquidity - the liquidity in the current tick range of the pool
+// tickLower, tickUpper - the min and max ticks of the current tick range
+// sPriceUpper, sPriceUpper - square roots of prices corresponding to the min and max ticks of the current range
+// tickSpacing - the tick spacing in the pool.
+// 	decimalsX, decimalsY - the number of decimals of the X and Y tokens, for printing the result
 export async function volToTarget(
 	token0: ERC20token,
 	token1: ERC20token,
 	pool: Contract,
+	s0: Slot0,
 	s: IRL,
 	sPriceTarget: number,
 ) {
 
 	//  amount of x in range; 
 	// sp = sPriceCurrent, sb = sPriceUpper
-	async function x_in_range(L: number, sp: number, sb: number) {
+	function x_in_range(L: number, sp: number, sb: number) {
 		return L * (sb - sp) / (sp * sb)
 	}
 
 	//  amount of y in range; 
 	// sp = sPriceCurrent, sa = sPriceLower
-	async function y_in_range(L: number, sp: number, sa: number) {
+	function y_in_range(L: number, sp: number, sa: number) {
 		return L * (sp - sa)
 	}
 
-	async function tick_to_price(tick: number) {
+	function tick_to_price(tick: number) {
 		return 1.0001 ** tick
 	}
 
-	let sPriceCurrent = s.sqrtPrice
-	let sPriceLower = s.sqrtRatioLow
-	let sPriceUpper = s.sqrtRatioHigh
+	let sPriceCurrent = Number(s.sqrtPrice)
+	let sPriceLower = Number(s.sqrtRatioLow)
+	let sPriceUpper = Number(s.sqrtRatioHigh)
 	let liquidity = Number(s.liquidity)
+	let tickLower = Number(s.tickLow)
+	let tickUpper = Number(s.tickHigh)
 
-	let Tick = {
+	let ITick = {
 		liquidityGross: 0,
 		liquidityNet: 0,
 		feeGrowthOutside0X128: 0,
@@ -71,53 +84,58 @@ export async function volToTarget(
 	let x = 0
 	if (sPriceTarget > sPriceCurrent) {
 		//  too few Y in the pool; we need to buy some X to increase amount of Y in pool
-		while (sPriceTarget > sPriceCurrent) {
+		if (sPriceTarget > sPriceCurrent) {
 			if (sPriceTarget > sPriceUpper)
 				//  not in the current price range; use all X in the range
-				x = await x_in_range(liquidity, sPriceCurrent, sPriceUpper)
+				x = x_in_range(liquidity, sPriceCurrent, sPriceUpper)
+
+
+			deltaTokens += x
+			//  query the blockchain for liquidity in the next tick range
+			let nextTickRange = await pool.ticks(s.tickHigh)
+			liquidity += Number(nextTickRange.liquidityNet)
+			// adjust the price and the range limits
+			sPriceCurrent = sPriceUpper
+			tickLower = tickUpper
+			tickUpper += Number(s.tickSpacing)
+			sPriceLower = sPriceUpper
+			sPriceUpper = tick_to_price(tickUpper / 2)
+
+
+		} else {
+			// in the current price range
+			x = x_in_range(liquidity, sPriceCurrent, sPriceTarget)
+			deltaTokens += x
+			sPriceCurrent = sPriceTarget
+			console.log("need to buy {:.10f} X tokens: ", (deltaTokens / 10 ** token0.decimals))
+
 		}
 	}
-	deltaTokens += x
-	//  query the blockchain for liquidity in the next tick range
-	let nextTickRange = Tick(* contract.functions.ticks(tickUpper).call())
-	liquidity += nextTickRange.liquidityNet
-	//  adjust the price and the range limits
-	sPriceCurrent = sPriceUpper
-	tickLower = tickUpper
-	tickUpper += tickSpacing
-	sPriceLower = sPriceUpper
-	sPriceUpper = tick_to_price(tickUpper // 2)
-        else:
-		//  in the current price range
-		x = x_in_range(liquidity, sPriceCurrent, sPriceTarget)
-            deltaTokens += x
-            sPriceCurrent = sPriceTarget
-    print("need to buy {:.10f} X tokens".format(deltaTokens / 10 ** decimalsX))
-
-elif sPriceTarget < sPriceCurrent:
-		//  too much Y in the pool; we need to buy some Y to decrease amount of Y in pool
-		currentTickRange = None
-	while sPriceTarget < sPriceCurrent:
-		if sPriceTarget < sPriceLower:
-			//  not in the current price range; use all Y in the range
-			y = y_in_range(liquidity, sPriceCurrent, sPriceLower)
-	deltaTokens += y
-	if currentTickRange is None:
-	//  query the blockchain for liquidityNet in the * current * tick range
-	currentTickRange = Tick(* contract.functions.ticks(tickLower).call())
-	liquidity -= currentTickRange.liquidityNet
-	//  adjust the price and the range limits
-	sPriceCurrent = sPriceLower
-	tickUpper = tickLower
-	tickLower -= tickSpacing
-	sPriceUpper = sPriceLower
-	sPriceLower = tick_to_price(tickLower // 2)
-            //  query the blockchain for liquidityNet in new current tick range
-	currentTickRange = Tick(* contract.functions.ticks(tickLower).call())
-        else:
-		//  in the current price range
-		y = y_in_range(liquidity, sPriceCurrent, sPriceTarget)
-	deltaTokens += y
-	sPriceCurrent = sPriceTarget
-	print("need to buy {:.10f} Y tokens".format(deltaTokens / 10 ** decimalsY))
+	if (sPriceTarget < sPriceCurrent) {
+		let currentTickRange: ITick = await pool.ticks(s0.tick)
+		if (sPriceTarget < sPriceCurrent) {
+			if (sPriceTarget < sPriceLower) {
+				//  not in the current price range; use all Y in the range
+				deltaTokens += y_in_range(liquidity, sPriceCurrent, sPriceLower)
+				if (currentTickRange.liquidityNet == 0n) {
+					//  query the blockchain for liquidity in the previous tick range
+					currentTickRange = await pool.ticks(s.tickLow)
+				}
+				liquidity -= Number(currentTickRange.liquidityNet)
+				// adjust the price and the range limits
+				sPriceCurrent = sPriceLower
+				tickUpper = tickLower
+				tickLower -= Number(s.tickSpacing)
+				sPriceUpper = sPriceLower
+				sPriceLower = tick_to_price(tickLower / 2)
+				currentTickRange = await pool.ticks(s.tickLow)
+			} else {
+				// in the current price range
+				let y = y_in_range(liquidity, sPriceCurrent, sPriceTarget)
+				deltaTokens += y
+				sPriceCurrent = sPriceTarget
+			}
+			console.log("need to buy {:.10f} Y tokens: ", (deltaTokens / 10 ** token1.decimals))
+		}
+	}
 }
